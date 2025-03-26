f4514d16dc2f590776df179d034b8f1fea44d525
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-26 15:29:46 +0300

    strict allowed methods

--- app/main.py
+++ app/main.py
@@ -8,8 +8,7 @@ app = fastapi.FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST"],
     allow_headers=["*"],
 )
 
